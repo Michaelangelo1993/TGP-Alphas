6afb7c3d3bf689692242f0f660cba95769e40f4e
ostngag <edward101@hotmail.co.uk>
2015-02-11 07:54:31 +0000

    Geiser Class and Menu Music
    Added Geiser class to master project
    Improved geiser texture
    Improved stalagmite texture
    Added lava wave texture
    Added Menu music
    Added Reset method to geiser class
    Attempted to get geiser animated //but not working

--- Game/Game/Geiser.cs
+++ Game/Game/Geiser.cs
@@ -15,26 +15,38 @@ namespace Game
 		private SpriteUV geiserSprite;
 		private TextureInfo geiserTextureInfo;
 		public float geiserPos;
+		private static float sizeX, sizeY, defaultXPos;
+		
+		private static int 			noOnGeiserSheetWidth, noOnGeiserSheetHeight;
+
 		
 		//Spike
 		private bool spikeBroken;
 		private SpriteUV spikeSprite;
 		private TextureInfo spikeTextureInfo;
 		public float spikeCurrentHeight;
-				
+		
+		
+		
 		public Geiser (Scene scene, Vector2 position)
 		{
 			spikeBroken = false;
-			
-			//Geiser sprite initialise
-			geiserTextureInfo = new TextureInfo("/Application/textures/Geiser.png");
+			sizeX 		= 116.6f;
+			sizeY		= 240.0f;
+
+			//Geiser sprite initialise /width of each geiser is 116.6px
+			geiserTextureInfo = new TextureInfo("/Application/textures/geiserSpriteSheet.png");
+			noOnGeiserSheetWidth 	= 5;
+			noOnGeiserSheetHeight 	= 1;
+			//defaultXPos				= ((textureInfo.TextureSizef.X/noOnGeiserSheetWidth)*1.00f)*0.5f;
 			geiserSprite = new SpriteUV(geiserTextureInfo);
+			//geiser.UV.S 			= new Vector2(1.0f/noOnGeiserSheetWidth,1.0f/noOnGeiserSheetHeight);
 			geiserSprite.Position = position;
-			geiserSprite.Quad.S = geiserTextureInfo.TextureSizef;
+			geiserSprite.Quad.S = new Vector2(sizeX, sizeY);
 			Bounds2 geiserBounds = geiserSprite.Quad.Bounds2();
 			
 			//Spike sprite initialise
-			spikeTextureInfo = new TextureInfo("/Application/textures/Spike.png");
+			spikeTextureInfo = new TextureInfo("/Application/textures/stalagmite.png");
 			spikeSprite = new SpriteUV(spikeTextureInfo);
 			spikeSprite.Position = position;
 			spikeSprite.Position = new Vector2(position.X+70, position.Y+450);
@@ -44,6 +56,11 @@ namespace Game
 			// Add sprites to scene
 			scene.AddChild(geiserSprite);
 			scene.AddChild(spikeSprite);
+		}
+		
+		public void Dispose()
+		{
+			
 		}
 		
 		public void BrokenSpike()
@@ -65,6 +82,8 @@ namespace Game
 		
 		public void Update()
 		{
+			//AnimatePlayer();
+			
 			if(spikeBroken == true)
 			{
 				//Check to see whether spike has reached the ground
@@ -76,6 +95,22 @@ namespace Game
 				}
 				
 			}
+		}
+		
+		private void AnimateGeiser()
+		{
+			if(spikeBroken = false)
+			{
+				//don't know what i'm doing
+			}
+		}
+		
+		public void Reset()
+		{
+			spikeBroken = false;
+			geiserSprite.Visible = true;
+			spikeSprite.Position = new Vector2((spikeSprite.Position.X + 2500), (spikeSprite.Position.Y));
+			geiserSprite.Position += new Vector2(2500, 0);
 		}
 	}
 }
